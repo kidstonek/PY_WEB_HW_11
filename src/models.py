6b8a4976a5d29b9b53019554328019be6fd1a40d
Andrii V <kidstonek@gmail.com>
2022-09-28 19:15:22 +0300

    Contacts add

--- src/models.py
+++ src/models.py
@@ -6,7 +6,7 @@ class Contact(db.Model):
     cont_id = db.Column(db.Integer, primary_key=True)
     cont_name = db.Column(db.String(60), nullable=False)
     cont_address = db.relationship("Address", back_populates="contact_ad")
-    cont_phone = db.relationship('Phone', back_populates='contact')
+    cont_phone = db.relationship('Phone', back_populates='contact_ad')
     cont_email = db.relationship('Email', back_populates='contact_email')
 
 
@@ -33,3 +33,18 @@ class Email(db.Model):
     email_mail = db.Column(db.String(60), nullable=False)
     contact_id = db.Column('contact_id', db.ForeignKey('contacts.cont_id', ondelete='CASCADE'), nullable=False)
     contact_email = db.relationship("Contact", back_populates="cont_email")
+
+
+class Test(db.Model):
+    __tablename__ = 'test'
+    test_id = db.Column(db.Integer, primary_key=True)
+    test_text = db.Column(db.String(120), nullable=False)
+    test_tri = db.relationship('Tri', back_populates='test_trir')
+
+
+class Tri(db.Model):
+    __tablename__ = 'tri'
+    tri_id = db.Column(db.Integer, primary_key=True)
+    tri_text = db.Column(db.String(120), nullable=False)
+    test_id = db.Column('test_id', db.ForeignKey('test.test_id', ondelete='CASCADE'), nullable=False)
+    test_trir = db.relationship("Test", back_populates="test_tri")
--- src/routes.py
+++ src/routes.py
@@ -1,4 +1,6 @@
 from . import app
+from flask import render_template, request, redirect, url_for
+from src.repository import testinput
 
 
 @app.route('/healthcheck', strict_slashes=False)
@@ -8,4 +10,29 @@ def healthcheck():
 
 @app.route('/', strict_slashes=False)
 def index():
-    return 'First page'
+    return render_template('pages/index.html', title='Notebook')
+
+
+@app.route('/contact', methods=['GET', 'POST'], strict_slashes=False)
+def create_contact():
+    if request.method == 'POST':
+        cname = request.form.get('name')
+        c_add = request.form.get('address')
+        c_phone = request.form.get('phone')
+        c_email = request.form.get('email')
+        contact_nn = testinput.contact_input(cname)
+        testinput.contact_details(c_add, c_phone, c_email, contact_nn.cont_id)
+        return redirect(url_for('index'))
+    return render_template('pages/contact.html', title='Add Contact')
+
+
+@app.route('/test', methods=['GET', 'POST'], strict_slashes=False)
+def test_input():
+    if request.method == 'POST':
+        text = request.form.get('text')
+        in_text = testinput.test_input(text)
+        print(in_text.test_id)
+        text_r = request.form.get('mri')
+        text_rel = testinput.test_input_rel(text_r, in_text.test_id)
+        return redirect(url_for('index'))
+    return render_template('pages/test.html', title='test input')
